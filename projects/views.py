ed450a7a50ac7a06c76f2943a0a28e3adcd0735c
Lukasz Chalinski <l.chalinski@gmail.com>
2020-12-30 17:10:25 +0100

    F005: add urls, first view, create template

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from projects.models import Project
+
+app_name = 'projects'
+
+class ProjectsListView(ListView):
+    template_name = "projects/all_projects.html"
+    model = Project
+
+    def get_queryset(self):
+        return self.model.objects.all()
